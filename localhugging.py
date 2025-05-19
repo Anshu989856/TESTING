b6e6a80ed735b12bde5073dad81319ee18cf24d2
ANSHU <anshu989856@gmail.com>
2025-05-19 16:34:30 +0530

    maybe last

--- localhugging.py
+++ localhugging.py
@@ -10,3 +10,13 @@ llm = HuggingFacePipeline.from_model_id(
 # Direct usage
 response = llm.invoke("Hannibal")
 print(response)
+
+from langchain_huggingface import ChatHuggingFace
+
+chat_model = ChatHuggingFace.from_model_id(
+    model_id="HuggingFaceH4/zephyr-7b-beta",
+    task="text-generation",
+    pipeline_kwargs={"temperature": 0.5, "max_new_tokens": 100}
+)
+response = chat_model.invoke([HumanMessage(content="Who was Hannibal and will graham?")])
+print(response.content)
